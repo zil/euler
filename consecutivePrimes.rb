641b9195184e7f8d01dd4a35eb3f326d20138cda
Vacker <fantanstic@gmail.com>
2012-08-14 19:47:31 +0800

    optimized

--- consecutivePrimes.rb
+++ consecutivePrimes.rb
@@ -17,30 +17,29 @@ def getPrimes
   end
 end
 
-def getMaxContinus(primes,index)
-  res=[index,0,0]
-  sum,idx = primes[index],index
-  if sum == 2
-    sum += primes[index + 1]
-    idx += 1
-  end
-  
-  while (idx < primes.size - 3) do
-    sum += primes[idx + 1]
-    sum += primes[idx + 2]
-    break if sum > primes[-1] 
-    res[1,2]=[idx+2,sum] if sum.prime? 
-    idx += 2
-  end
+def getMaxContinus(primes,start)
+  res=[start,0,0]
+  return res if start > primes.size - 4
+
+  start.upto(primes.size-4){|index|
+    sum,idx = primes[index],index
+    if index==0
+      idx=1;
+      sum += primes[1]
+    end
+
+    while (idx < primes.size - 3) do
+      sum += primes[idx + 1]
+      sum += primes[idx + 2]
+      break if sum > primes[-1]
+      res=[index,idx+2,sum] if ((idx+2-index) > res[1]-res[0] ) && sum.prime? 
+      idx += 2
+    end
+  }
   res
 end
 
-maxLength=[0,3,0]
 primes=getPrimes
-
-1.upto(primes.size - 3){|idx|
-  res=getMaxContinus(primes,idx)
-  next if not res
-  maxLength = res if (res[1] - res[0]) > (maxLength[1] - maxLength[0])
-}
-puts maxLength
+res=getMaxContinus(primes,0)
+puts res
+puts (res[0]..(res[0]+res[1]-1)).inject(0){|su,r|su + primes[r]}
