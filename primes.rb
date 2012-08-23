1f4dfacb1bcced3b3755f6d4c608f90a1857c291
Vacker <fantanstic@gmail.com>
2012-08-23 12:28:31 +0800

    fixs miss prime[2,5] error

--- primes.rb
+++ primes.rb
@@ -5,9 +5,9 @@ def getPrimes
     savedPrimes=Marshal.load(pf)
     return savedPrimes if savedPrimes
   rescue
-    p=[]
-    2.upto(10**6){|n|
-      next if n.even? || n % 5==0
+    p=[2]
+    3.upto(10**6){|n|
+      next if n.even?
       p << n if n.prime?
     }
     Marshal.dump(p,File.new("primes.tmp",'w'))
